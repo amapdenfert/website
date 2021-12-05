83e173d48c42bd3c3f4737aa8bbed313f02890d9
amapdenfert <95582249+amapdenfert@users.noreply.github.com>
2021-12-05 17:43:51 +0100

    Batch update

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":980},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-contact","displayName":"Contact","link":{"linkType":"LinkTypePage","href":"#!page-contact"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-informations","displayName":"Informations","link":{"linkType":"LinkTypePage","href":"#!page-informations"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true},{"id":"page-producteurs","displayName":"Producteurs","link":{"linkType":"LinkTypePage","href":"#!page-producteurs"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true}]}
